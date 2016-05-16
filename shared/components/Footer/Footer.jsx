c876c3918f63b65a1952fc3652b1d3e117393142
Md. Mahfuzur Rahman <mahfuz@student.sust.edu>
2016-05-16 12:58:41 +0600

    Update Footer.jsx

--- shared/components/Footer/Footer.jsx
+++ shared/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function Footer() {
   return (
    <div className="footer">
+      <p>Developed By: <a href="https://twitter.com/mahfuzsust" target="_Blank">mahfuzsust</a></p>
      <p>&copy; 2016 &middot; Hashnode &middot; LinearBytes Inc.</p>
      <p>We are on Twitter : <a href="https://twitter.com/@mern_io" target="_Blank">@mern_io</a></p>
    </div>
